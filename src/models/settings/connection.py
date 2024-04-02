8f412eb9431adeac4c0baa8dbc68f1e5fb275d88
FelipeAlvesx <feelipe.devloper@gmail.com>
2024-04-02 14:18:54 -0300

    connection db

---
+++ src/models/settings/connection.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+
+class DBConnectionHandler:  
+    
+    def __init__(self):
+        self.__connection_string = "{}:///{}".format(
+            "sqlite",
+            "storage(1).db"
+        )    
+        self.__engine = None
+    
+    def connect_to_db(self) -> None:
+        self.__engine = create_engine(self.__connection_string)
+
+    def get_engine(self):
+        return self.__engine
